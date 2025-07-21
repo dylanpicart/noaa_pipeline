cf342e85da2324e53b04364176d9ce3fd4b9fbdd
dylanpicart <dylanpicart@mail.adelphi.edu>
2025-07-21 17:42:59 -0400

    Add fetch_chlorophyll_data import to utils module

--- graphql/utils/__init__.py
+++ graphql/utils/__init__.py
@@ -1,3 +1,3 @@
 # utils/__init__.py
-from .api_requests import fetch_pmn_data, fetch_buoy_data, fetch_climate_data
+from .api_requests import fetch_pmn_data, fetch_buoy_data, fetch_climate_data, fetch_chlorophyll_data
 from .logging_setup import setup_logging
